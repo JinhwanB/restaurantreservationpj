0848bfc62966bd2e868b9ae5a5e930d8a89608fa
jinhwan Bea <paewhal20@gmail.com>
2024-05-20 19:29:08 +0900

    Feat: 생성 날짜 / 수정 날짜 자동화 기능 클래스 구현
    해당 클래스를 엔티티에서 상속하여 사용하면 각 엔티티별 생성 날짜와 수정 날짜를 자동으로 필드에 업데이트 된다.

---
+++ src/main/java/com/jh/restaurantreservationpj/config/BaseTimeEntity.java
@@ -0,0 +1,25 @@
+package com.jh.restaurantreservationpj.config;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @Column(updatable = false, nullable = false)
+    @CreatedDate
+    private LocalDateTime regDate; // 생성 날짜
+
+    @Column(nullable = false)
+    @LastModifiedDate
+    private LocalDateTime chgDate; // 수정 날짜
+}
