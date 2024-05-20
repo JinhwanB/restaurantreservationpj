fdf40f4678331dec5b01afbd6574963ba634bc4a
jinhwan Bea <paewhal20@gmail.com>
2024-05-20 18:29:02 +0900

    first init

---
+++ src/main/java/com/jh/restaurantreservationpj/RestaurantreservationpjApplication.java
@@ -0,0 +1,13 @@
+package com.jh.restaurantreservationpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantreservationpjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantreservationpjApplication.class, args);
+	}
+
+}
