82d43a8f38ca009b7eddcdef5771382c1e4f6ce4
jinhwan Bea <paewhal20@gmail.com>
2024-05-21 18:36:17 +0900

    Update RestaurantErrorCode.java
    이미 존재하는 매장명인 경우의 errorCode 추가

--- src/main/java/com/jh/restaurantreservationpj/restaurant/exception/RestaurantErrorCode.java
+++ src/main/java/com/jh/restaurantreservationpj/restaurant/exception/RestaurantErrorCode.java
@@ -6,8 +6,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum RestaurantErrorCode {
+    ALREADY_EXIST_NAME(400, "이미 존재하는 매장입니다."),
     NOT_VALID_ARGS_OF_OPEN_TIME_AND_CLOSE_TIME(400, "오픈 시간과 마감 시간은 둘 다 null이거나 둘 다 유효한 값이 있어야 합니다.");
-    
+
     private final int status;
     private final String message;
 }
