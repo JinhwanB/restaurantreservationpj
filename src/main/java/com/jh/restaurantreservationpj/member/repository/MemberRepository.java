0e2c472b7809c9237701be68eb30ebd44773c032
jinhwan Bea <paewhal20@gmail.com>
2024-05-21 13:53:49 +0900

    Feat: 회원 아이디를 통해 회원 엔티티 찾는 쿼리메소드 추가

---
+++ src/main/java/com/jh/restaurantreservationpj/member/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.jh.restaurantreservationpj.member.repository;
+
+import com.jh.restaurantreservationpj.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUserId(String userId); // 회원 아이디를 통해 회원 찾기
+}
