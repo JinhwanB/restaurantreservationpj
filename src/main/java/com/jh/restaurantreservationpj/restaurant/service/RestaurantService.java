9be47e2b9bdce86aff8aca42f5ddcfc28666db78
jinhwan Bea <paewhal20@gmail.com>
2024-05-21 18:38:43 +0900

    Refector: 이미 존재하는 매장 이름의 경우 exception

--- src/main/java/com/jh/restaurantreservationpj/restaurant/service/RestaurantService.java
+++ src/main/java/com/jh/restaurantreservationpj/restaurant/service/RestaurantService.java
@@ -6,29 +6,33 @@ import com.jh.restaurantreservationpj.member.exception.MemberException;
 import com.jh.restaurantreservationpj.member.repository.MemberRepository;
 import com.jh.restaurantreservationpj.restaurant.domain.Restaurant;
 import com.jh.restaurantreservationpj.restaurant.dto.CreateRestaurantDto;
+import com.jh.restaurantreservationpj.restaurant.exception.RestaurantErrorCode;
+import com.jh.restaurantreservationpj.restaurant.exception.RestaurantException;
 import com.jh.restaurantreservationpj.restaurant.repository.RestaurantRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
-@Slf4j
 public class RestaurantService {
 
     private final RestaurantRepository restaurantRepository;
     private final MemberRepository memberRepository;
 
     /**
-     * 매장 등록
-     * @param request
-     * @return 저장한 엔티티를 response로 변경
+     * 매장 등록 서비스
      */
     public CreateRestaurantDto.Response createRestaurant(CreateRestaurantDto.Request request){
         String userId = request.getUserId();
         Member manager = memberRepository.findByUserId(userId).orElseThrow(() -> new MemberException(MemberErrorCode.NOT_FOUND_MEMBER));
+
+        String name = request.getName();
+        if(restaurantRepository.existsByName(name)){
+            throw new RestaurantException(RestaurantErrorCode.ALREADY_EXIST_NAME);
+        }
+        
         Restaurant restaurant = Restaurant.builder()
                 .manager(manager)
                 .name(request.getName())
