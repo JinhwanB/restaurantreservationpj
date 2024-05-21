130d332d043287f75375b4904ef760ca657083f5
jinhwan Bea <paewhal20@gmail.com>
2024-05-21 13:24:39 +0900

    Chore: 어노테이션 추가
    문서에 적용되는 어노테이션 추가

--- src/main/java/com/jh/restaurantreservationpj/validation/NotNullPattern.java
+++ src/main/java/com/jh/restaurantreservationpj/validation/NotNullPattern.java
@@ -3,11 +3,9 @@ package com.jh.restaurantreservationpj.validation;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
+@Documented
 @Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = NotNullPatternValidator.class)
