ebf1647f7e2728f0631f283c9fe1e89cea53fbaf
jinhwan Bea <paewhal20@gmail.com>
2024-05-21 20:51:37 +0900

    Update RestaurantRepository.java
    Feat: 매장 이름으로 엔티티 찾는 쿼리메소드 추가

--- src/main/java/com/jh/restaurantreservationpj/restaurant/repository/RestaurantRepository.java
+++ src/main/java/com/jh/restaurantreservationpj/restaurant/repository/RestaurantRepository.java
@@ -4,7 +4,11 @@ import com.jh.restaurantreservationpj.restaurant.domain.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
     boolean existsByName(String name); // 매장 이름이 존재하는지 여부 확인 쿼리 메소드
+
+    Optional<Restaurant> findByName(String name); // 매장 이름으로 엔티티 찾기
 }
