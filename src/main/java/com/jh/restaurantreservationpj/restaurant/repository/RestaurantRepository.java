49c8e642b7d07be30c41969edeed8986c75b1d5f
jinhwan Bea <paewhal20@gmail.com>
2024-05-21 18:34:04 +0900

    Feat: 매장 이름이 중복인지 확인하는 쿼리 메소드 추가

--- src/main/java/com/jh/restaurantreservationpj/restaurant/repository/RestaurantRepository.java
+++ src/main/java/com/jh/restaurantreservationpj/restaurant/repository/RestaurantRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
-    
+    boolean existsByName(String name); // 매장 이름이 존재하는지 여부 확인 쿼리 메소드
 }
