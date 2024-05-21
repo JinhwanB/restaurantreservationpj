325e2e33750aa3a9ca538da775b44834a03a20f3
jinhwan Bea <paewhal20@gmail.com>
2024-05-21 12:15:20 +0900

    Feat: null값을 허용하는 커스텀 validation 어노테이션 추가
    null값을 허용하면서 만약 null이 아니라면 알맞은 패턴 값으로 들어왔는지 확인하는 어노테이션

---
+++ src/main/java/com/jh/restaurantreservationpj/validation/NotNullPatternValidator.java
@@ -0,0 +1,25 @@
+package com.jh.restaurantreservationpj.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.regex.Pattern;
+
+public class NotNullPatternValidator implements ConstraintValidator<NotNullPattern, String> {
+
+    private Pattern pattern;
+
+    @Override
+    public void initialize(NotNullPattern constraintAnnotation) {
+        this.pattern = Pattern.compile(constraintAnnotation.pattern()); // 패턴 초기화
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if(value == null || value.trim().isEmpty()){ // null 또는 빈값인 경우 허용
+            return true;
+        }
+
+        return pattern.matcher(value).matches();
+    }
+}
