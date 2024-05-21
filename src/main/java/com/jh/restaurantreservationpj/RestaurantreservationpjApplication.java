798d046c0df2c544452dc3115c8651700f787a49
jinhwan Bea <paewhal20@gmail.com>
2024-05-21 18:53:01 +0900

    Feat: AOP 활성화

--- src/main/java/com/jh/restaurantreservationpj/RestaurantreservationpjApplication.java
+++ src/main/java/com/jh/restaurantreservationpj/RestaurantreservationpjApplication.java
@@ -2,10 +2,12 @@ package com.jh.restaurantreservationpj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableAspectJAutoProxy
 public class RestaurantreservationpjApplication {
 
 	public static void main(String[] args) {
