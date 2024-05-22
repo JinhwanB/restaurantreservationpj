c0b4c3db0f3c30eca5685ef177f5f35c147f37b8
jinhwan Bea <paewhal20@gmail.com>
2024-05-22 14:54:12 +0900

    Update ReservationRepository.java
    Feat: 매장에 해당하는 예약 리스트 조회 쿼리 메소드

--- src/main/java/com/jh/restaurantreservationpj/reservation/repository/ReservationRepository.java
+++ src/main/java/com/jh/restaurantreservationpj/reservation/repository/ReservationRepository.java
@@ -1,12 +1,16 @@
 package com.jh.restaurantreservationpj.reservation.repository;
 
 import com.jh.restaurantreservationpj.reservation.domain.Reservation;
+import com.jh.restaurantreservationpj.restaurant.domain.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     Optional<Reservation> findByReservationNumber(String reservationNumber); // 예약 번호를 통해 예약 찾기
+
+    List<Reservation> findAllByReservationRestaurant(Restaurant restaurant); // 매장에 해당하는 예약 리스트 가져오기
 }
