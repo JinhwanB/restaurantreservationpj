eefc57817e11cdce90fa0bb4ef12cafac059afb7
jinhwan Bea <paewhal20@gmail.com>
2024-05-22 14:28:32 +0900

    Create Reservation.java
    Feat: 예약 엔티티

---
+++ src/main/java/com/jh/restaurantreservationpj/reservation/domain/Reservation.java
@@ -0,0 +1,40 @@
+package com.jh.restaurantreservationpj.reservation.domain;
+
+import com.jh.restaurantreservationpj.member.domain.Member;
+import com.jh.restaurantreservationpj.restaurant.domain.Restaurant;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String reservationNumber; // 예약 번호
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_number", nullable = false)
+    private Member reservationId; // 예약한 회원의 아이디
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    private Restaurant reservationRestaurant; // 예약한 식당
+
+    @Column(nullable = false)
+    private String reservationTime; // 희망 예약 시간
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ReservationState reservationState; // 점장 예약 승인 여부
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ReservationVisit reservationVisit; // 방문 확인 여부
+}
