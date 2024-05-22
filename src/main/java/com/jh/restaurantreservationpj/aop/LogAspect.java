6d489c18e23969c946237d8720093120d3c4eff4
jinhwan Bea <paewhal20@gmail.com>
2024-05-22 11:50:04 +0900

    Update LogAspect.java
    Refector: 로그 찍을 시 해당 클래스 위치 표시 및 파라미터로 들어온 값 표시

--- src/main/java/com/jh/restaurantreservationpj/aop/LogAspect.java
+++ src/main/java/com/jh/restaurantreservationpj/aop/LogAspect.java
@@ -9,6 +9,7 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 @Aspect
@@ -18,38 +19,60 @@ public class LogAspect {
 
     // 모든 메소드 포인트컷
     @Pointcut("execution(* com.jh.restaurantreservationpj..*(..))")
-    public void all(){}
+    public void all() {
+    }
 
     // Controller클래스의 모든 메소드 포인트컷
     @Pointcut("execution(* com.jh.restaurantreservationpj..*Controller.*(..))")
-    public void controller(){}
+    public void controller() {
+    }
 
     // Service클래스의 모든 메소드 포인트컷
     @Pointcut("execution(* com.jh.restaurantreservationpj..*Service.*(..))")
-    public void service(){}
+    public void service() {
+    }
 
     // Service 또는 Controller의 메소드 실행 전 로그 표시
     @Before("controller() || service()")
-    public void beforeLog(JoinPoint joinPoint){
+    public void beforeLog(JoinPoint joinPoint) {
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
         Method method = signature.getMethod();
-        log.info("{}메소드 실행", method.getName());
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        log.info("[{}] {}메소드 실행", className, method.getName());
 
         Object[] args = joinPoint.getArgs();
         for (Object arg : args) {
-            if(arg != null){
-                log.info("파라미터 타입 = {}", arg.getClass().getSimpleName());
-                log.info("파라미터 값 = {}", arg);
+            if (arg != null) {
+                if (arg.getClass().getPackage().getName().startsWith("com.jh.restaurantreservationpj")) {
+                    log.info("파라미터 타입 = {}", arg.getClass().getName());
+
+                    // 객체 파라미터 필드 값 로그 출력
+                    Field[] fields = arg.getClass().getDeclaredFields();
+                    for (Field field : fields) {
+                        field.setAccessible(true);  // private 필드 접근 허용
+                        try {
+                            Object fieldValue = field.get(arg);
+                            log.info("필드 이름 = {} -> {}", field.getName(), fieldValue);
+                        } catch (IllegalAccessException e) {
+                            log.error("필드 값을 가져올 수 없음", e);
+                        }
+                    }
+                    
+                    continue;
+                }
+
+                log.info("파라미터 타입 = {} -> {}", arg.getClass().getSimpleName(), arg);
             }
         }
     }
 
     // 모든 메소드에서 예외 발생시 로그 표시
     @AfterThrowing(pointcut = "all()", throwing = "ex")
-    public void afterThrowingLog(JoinPoint joinPoint, Throwable ex){
+    public void afterThrowingLog(JoinPoint joinPoint, Throwable ex) {
         MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
         Method method = methodSignature.getMethod();
-        log.error("Exception in method = {}", method.getName());
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        log.error("[{}] {}메소드에서 예외 발생", className, method.getName());
         log.error("Exception message = {}", ex.getMessage());
     }
 }
