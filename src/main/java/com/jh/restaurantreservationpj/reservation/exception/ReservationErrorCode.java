7f6bb0e489338bc4921b05fffcc4df9f358a8f8d
jinhwan Bea <paewhal20@gmail.com>
2024-05-22 16:58:32 +0900

    Update ReservationErrorCode.java
    Feat: 예약 불가능한 시간 입력할 시 errorcode추가

--- src/main/java/com/jh/restaurantreservationpj/reservation/exception/ReservationErrorCode.java
+++ src/main/java/com/jh/restaurantreservationpj/reservation/exception/ReservationErrorCode.java
@@ -6,8 +6,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ReservationErrorCode {
+    IMPOSSIBLE_RESERVATION(400, "해당 시간에 예약할 수 없습니다."),
     NOT_FOUND_RESERVATION(400, "예약 정보가 없습니다.");
-    
+
     private final int status;
     private final String message;
 }
