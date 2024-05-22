2314cf5927163b5d5f970574903ec8e58794e83d
jinhwan Bea <paewhal20@gmail.com>
2024-05-22 11:50:39 +0900

    Update RestaurantControllerTest.java
    Test: 매장 수정 컨트롤러 테스트

--- src/test/java/com/jh/restaurantreservationpj/restaurant/controller/RestaurantControllerTest.java
+++ src/test/java/com/jh/restaurantreservationpj/restaurant/controller/RestaurantControllerTest.java
@@ -6,6 +6,9 @@ import com.jh.restaurantreservationpj.member.domain.MemberRole;
 import com.jh.restaurantreservationpj.member.domain.Role;
 import com.jh.restaurantreservationpj.member.repository.MemberRepository;
 import com.jh.restaurantreservationpj.restaurant.dto.CreateRestaurantDto;
+import com.jh.restaurantreservationpj.restaurant.dto.ModifiedRestaurantDto;
+import com.jh.restaurantreservationpj.restaurant.exception.RestaurantErrorCode;
+import com.jh.restaurantreservationpj.restaurant.service.RestaurantService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,6 +23,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -32,15 +36,19 @@ class RestaurantControllerTest {
     private MemberRepository memberRepository;
 
     @Autowired
+    private RestaurantService restaurantService;
+
+    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
 
-    CreateRestaurantDto.Request request;
+    CreateRestaurantDto.Request createRequest;
+    ModifiedRestaurantDto.Request modifyRequest;
 
     @BeforeEach
-    void before(){
+    void before() {
         MemberRole memberRole = MemberRole.builder()
                 .role(Role.ROLE_ADMIN)
                 .build();
@@ -56,10 +64,19 @@ class RestaurantControllerTest {
 
         memberRepository.save(member);
 
-        request = CreateRestaurantDto.Request.builder()
+        createRequest = CreateRestaurantDto.Request.builder()
                 .userId("test")
                 .name("매장 이름")
                 .description("설명")
+                .openTime("08")
+                .closeTime("22")
+                .totalAddress("주소")
+                .build();
+
+        modifyRequest = ModifiedRestaurantDto.Request.builder()
+                .userId("test")
+                .name("매장 이름")
+                .description("설명2")
                 .openTime("08")
                 .closeTime("22")
                 .totalAddress("주소")
@@ -71,7 +88,7 @@ class RestaurantControllerTest {
     void register() throws Exception {
         mockMvc.perform(post("/restaurants/restaurant")
                         .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(request)))
+                        .content(objectMapper.writeValueAsString(createRequest)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.status").value(200))
                 .andExpect(jsonPath("$.message").value("성공"))
@@ -81,7 +98,7 @@ class RestaurantControllerTest {
     @Test
     @DisplayName("매장 등록 컨트롤러 실패 - 유효성 검증 실패 1")
     void failRegister1() throws Exception {
-        CreateRestaurantDto.Request modifiedRequest = request.toBuilder()
+        CreateRestaurantDto.Request modifiedRequest = createRequest.toBuilder()
                 .name(null)
                 .build();
 
@@ -95,7 +112,7 @@ class RestaurantControllerTest {
     @Test
     @DisplayName("매장 등록 컨트롤러 실패 - 유효성 검증 실패 2")
     void failRegister2() throws Exception {
-        CreateRestaurantDto.Request modifiedRequest = request.toBuilder()
+        CreateRestaurantDto.Request modifiedRequest = createRequest.toBuilder()
                 .openTime("088")
                 .build();
 
@@ -109,7 +126,7 @@ class RestaurantControllerTest {
     @Test
     @DisplayName("매장 등록 컨트롤러 실패 - 유효성 검증 실패 3")
     void failRegister3() throws Exception {
-        CreateRestaurantDto.Request modifiedRequest = request.toBuilder()
+        CreateRestaurantDto.Request modifiedRequest = createRequest.toBuilder()
                 .openTime(null)
                 .build();
 
@@ -118,5 +135,64 @@ class RestaurantControllerTest {
                         .content(objectMapper.writeValueAsString(modifiedRequest)))
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.status").value(400));
+    }
+
+    @Test
+    @DisplayName("매장 수정 컨트롤러")
+    void modify() throws Exception {
+        restaurantService.createRestaurant(createRequest);
+
+        mockMvc.perform(put("/restaurants/restaurant/매장 이름")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(modifyRequest)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value(200))
+                .andExpect(jsonPath("$.message").value("성공"))
+                .andExpect(jsonPath("$.data").exists());
+    }
+
+    @Test
+    @DisplayName("매장 수정 컨트롤러 실패 - 유효성 검증 실패 1")
+    void failModify1() throws Exception {
+        restaurantService.createRestaurant(createRequest);
+
+        mockMvc.perform(put("/restaurants/restaurant/ ")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(modifyRequest)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$[0].status").value(400));
+    }
+
+    @Test
+    @DisplayName("매장 수정 컨트롤러 실패 - 유효성 검증 실패 2")
+    void failModify2() throws Exception {
+        restaurantService.createRestaurant(createRequest);
+
+        ModifiedRestaurantDto.Request badRequest = modifyRequest.toBuilder()
+                .name(null)
+                .build();
+
+        mockMvc.perform(put("/restaurants/restaurant/매장 이름")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(badRequest)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$[0].status").value(400));
+    }
+
+    @Test
+    @DisplayName("매장 수정 컨트롤러 실패 - 유효성 검증 실패 3")
+    void failModify3() throws Exception {
+        restaurantService.createRestaurant(createRequest);
+
+        ModifiedRestaurantDto.Request badRequest = modifyRequest.toBuilder()
+                .openTime(null)
+                .build();
+
+        mockMvc.perform(put("/restaurants/restaurant/매장 이름")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(badRequest)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value(400))
+                .andExpect(jsonPath("$.message").value(RestaurantErrorCode.NOT_VALID_ARGS_OF_OPEN_TIME_AND_CLOSE_TIME.getMessage()));
     }
 }
