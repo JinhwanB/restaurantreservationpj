e299306e786d595cd81a3fa2d6d58a91226582de
jinhwan Bea <paewhal20@gmail.com>
2024-05-22 12:32:22 +0900

    Update RestaurantRepository.java
    Feat: 검색한 문자로 시작하거나 검색한 문자를 포함하는 매장을 오름차순으로 정렬하여 가져오는 쿼리 메소드

--- src/main/java/com/jh/restaurantreservationpj/restaurant/repository/RestaurantRepository.java
+++ src/main/java/com/jh/restaurantreservationpj/restaurant/repository/RestaurantRepository.java
@@ -1,6 +1,7 @@
 package com.jh.restaurantreservationpj.restaurant.repository;
 
 import com.jh.restaurantreservationpj.restaurant.domain.Restaurant;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +12,6 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
     boolean existsByName(String name); // 매장 이름이 존재하는지 여부 확인 쿼리 메소드
 
     Optional<Restaurant> findByName(String name); // 매장 이름으로 엔티티 찾기
+
+    Page<Restaurant> findAllByNameStartingWithIgnoreCaseOrNameContainingIgnoreCaseOrderByNameAsc(String prefix1, String prefix2); // 검색한 문자로 시작하거나 검색한 문자를 포함하는 매장을 오름차순으로 정렬하여 가져오는 쿼리 메소드
 }
