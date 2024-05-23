691b3f4be56febda7bf0328dc0439e085055f209
jinhwan Bea <paewhal20@gmail.com>
2024-05-23 20:32:39 +0900

    Update ReservationService.java
    Feat: 문자열 시간을 LocalDateTime으로 변경하는 메소드

--- src/main/java/com/jh/restaurantreservationpj/reservation/service/ReservationService.java
+++ src/main/java/com/jh/restaurantreservationpj/reservation/service/ReservationService.java
@@ -50,10 +50,26 @@ public class ReservationService {
                 .reservationMember(member)
                 .reservationRestaurant(restaurant)
                 .reservationTime(request.getTime().trim())
+                .isCancel(false)
                 .build();
         Reservation save = reservationRepository.save(reservation);
 
         return save.toCreateResponse();
+    }
+
+    /*
+    회원이 예약 취소하는 서비스
+    예약 수정은 불가 대신 취소 가능
+    취소는 예약 시간 1시간 전까지만 가능
+     */
+    public String cancelFromMemberReservation(String memberId, String reservationNumber) {
+        Member member = memberRepository.findByUserId(memberId).orElseThrow(() -> new MemberException(MemberErrorCode.NOT_FOUND_MEMBER));
+
+        Reservation reservation = reservationRepository.findByReservationNumber(reservationNumber).orElseThrow(() -> new ReservationException(ReservationErrorCode.NOT_FOUND_RESERVATION));
+        Member reservationMember = reservation.getReservationMember();
+        if (member != reservationMember) { // 예약 정보의 회원과 다른 경우
+            throw new ReservationException(ReservationErrorCode.DIFF_RESERVATION_MEMBER);
+        }
     }
 
     // 점장이 매장 예약 목록을 확인하는 서비스
@@ -89,21 +105,27 @@ public class ReservationService {
     // 매장의 오픈 시간과 마감 시간 사이의 시간으로 예약을 했는지 확인
     private void validReservationTime(Restaurant restaurant, String reservationTime) {
         LocalDateTime now = LocalDateTime.now();
-        int year = now.getYear();
-        int month = now.getMonth().getValue();
-        int day = now.getDayOfMonth();
 
-        int openTime = Integer.parseInt(restaurant.getOpenTime());
-        int closeTime = Integer.parseInt(restaurant.getCloseTime());
-        LocalDateTime restaurantOpenTime = LocalDateTime.of(year, month, day, openTime, 0); // 매장 오픈 시간
-        LocalDateTime restaurantCloseTime = LocalDateTime.of(year, month, day, closeTime, 0); // 매장 마감 시간
+        LocalDateTime restaurantOpenTime = stringToLocalDateTime(restaurant.getOpenTime()); // 매장 오픈 시간
+        LocalDateTime restaurantCloseTime = stringToLocalDateTime(restaurant.getCloseTime()); // 매장 마감 시간
 
-        int timeOfReservation = Integer.parseInt(reservationTime);
-        LocalDateTime hopeTime = LocalDateTime.of(year, month, day, timeOfReservation, 0); // 희망 예약 시간
+        LocalDateTime hopeTime = stringToLocalDateTime(reservationTime); // 희망 예약 시간
 
         // 예약하고자 하는 시간이 현재 시간보다 이전이거나 매장 오픈시간보다 이전인지 또는 매장 마감시간 이후인지 확인
         if (hopeTime.isBefore(now) || hopeTime.isBefore(restaurantOpenTime) || hopeTime.isAfter(restaurantCloseTime)) {
             throw new ReservationException(ReservationErrorCode.IMPOSSIBLE_RESERVATION);
         }
+    }
+
+    // 문자열 시간을 LocalDateTime으로 변경
+    private LocalDateTime stringToLocalDateTime(String time) {
+        LocalDateTime now = LocalDateTime.now();
+        int year = now.getYear();
+        int month = now.getMonth().getValue();
+        int day = now.getDayOfMonth();
+
+        int stringToTime = Integer.parseInt(time);
+
+        return LocalDateTime.of(year, month, day, stringToTime, 0);
     }
 }
