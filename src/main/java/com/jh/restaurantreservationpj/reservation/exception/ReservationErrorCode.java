4aed0b3512027e27b44164a6f9dc33f0c22499ea
jinhwan Bea <paewhal20@gmail.com>
2024-05-23 21:34:05 +0900

    Update ReservationErrorCode.java
    Feat: 이미 완료된 예약인 경우, 이미 취소된 예약인 경우, 이미 예약이 거절된 경우, 예약 취소를 예약 시간 1시간 전 이후에 하려고 하는 경우의 에러 코드 추가

--- src/main/java/com/jh/restaurantreservationpj/reservation/exception/ReservationErrorCode.java
+++ src/main/java/com/jh/restaurantreservationpj/reservation/exception/ReservationErrorCode.java
@@ -6,6 +6,10 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ReservationErrorCode {
+    ALREADY_USED_RESERVATION(400, "이미 완료(매장 방문)된 예약입니다."),
+    ALREADY_CANCELED_RESERVATION(400, "이미 취소된 예약입니다."),
+    ALREADY_DENIED_RESERVATION(400, "이미 예약 거절되었습니다."),
+    IMPOSSIBLE_CANCEL(400, "예약 취소는 예약한 시간 1시간 전까지만 가능합니다."),
     DIFF_RESERVATION_MEMBER(400, "예약한 회원과 다른 회원입니다."),
     IMPOSSIBLE_RESERVATION(400, "해당 시간에 예약할 수 없습니다."),
     NOT_FOUND_RESERVATION(400, "예약 정보가 없습니다.");
