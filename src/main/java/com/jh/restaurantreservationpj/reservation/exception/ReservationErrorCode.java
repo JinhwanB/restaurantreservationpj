c0809cdef77adaccbfafd81009b4724204f3fe53
jinhwan Bea <paewhal20@gmail.com>
2024-05-23 20:25:14 +0900

    Update ReservationErrorCode.java
    Feat: 예약을 진행한 회원과 다른 경우 에러 코드 추가

--- src/main/java/com/jh/restaurantreservationpj/reservation/exception/ReservationErrorCode.java
+++ src/main/java/com/jh/restaurantreservationpj/reservation/exception/ReservationErrorCode.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ReservationErrorCode {
+    DIFF_RESERVATION_MEMBER(400, "예약한 회원과 다른 회원입니다."),
     IMPOSSIBLE_RESERVATION(400, "해당 시간에 예약할 수 없습니다."),
     NOT_FOUND_RESERVATION(400, "예약 정보가 없습니다.");
 
