a38d4a8238552a628b1f594b673f086facdcefc9
jinhwan Bea <paewhal20@gmail.com>
2024-05-24 14:41:25 +0900

    Create DenyReservationDto.java
    Feat: 예약 거절 시 dto

---
+++ src/main/java/com/jh/restaurantreservationpj/reservation/dto/DenyReservationDto.java
@@ -0,0 +1,22 @@
+package com.jh.restaurantreservationpj.reservation.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+
+public class DenyReservationDto {
+
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor
+    @Builder(toBuilder = true)
+    public static class Request {
+
+        @NotBlank(message = "예약 번호를 입력해주세요.")
+        @Pattern(regexp = "\\d{8}", message = "예약 번호는 8자리 숫자입니다.")
+        private String reservationNumber; // 예약 번호
+
+        @NotBlank(message = "예약 거절 사유를 입력해주세요.")
+        private String reason; // 예약 거절 사유
+    }
+}
