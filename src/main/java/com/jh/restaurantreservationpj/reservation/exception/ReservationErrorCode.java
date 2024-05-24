0769661458fb3323fcdf08a5310d31d2b71bf42d
jinhwan Bea <paewhal20@gmail.com>
2024-05-24 14:47:43 +0900

    Update ReservationErrorCode.java
    Feat: 예약한 매장의 관리자와 다른 관리자일 경우 에러 코드 추가

--- src/main/java/com/jh/restaurantreservationpj/reservation/exception/ReservationErrorCode.java
+++ src/main/java/com/jh/restaurantreservationpj/reservation/exception/ReservationErrorCode.java
@@ -12,6 +12,7 @@ public enum ReservationErrorCode {
     ALREADY_CANCELED_RESERVATION(400, "이미 취소된 예약입니다."),
     ALREADY_DENIED_RESERVATION(400, "이미 예약 거절되었습니다."),
     IMPOSSIBLE_CANCEL(400, "예약 취소는 예약한 시간 1시간 전까지만 가능합니다."),
+    DIFF_RESERVATION_MANAGER(400, "예약한 식당의 관리자와 다른 관리자입니다."),
     DIFF_RESERVATION_MEMBER(400, "예약한 회원과 다른 회원입니다."),
     IMPOSSIBLE_RESERVATION(400, "해당 시간에 예약할 수 없습니다."),
     NOT_FOUND_RESERVATION(400, "예약 정보가 없습니다.");
