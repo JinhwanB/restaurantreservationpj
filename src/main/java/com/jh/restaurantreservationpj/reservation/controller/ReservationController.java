4d124e87fbe6f89cc09db116da6fa8a854c9c873
jinhwan Bea <paewhal20@gmail.com>
2024-05-28 17:08:44 +0900

    Update ReservationController.java
    Feat: 회원이 예약 취소하는 컨트롤러

--- src/main/java/com/jh/restaurantreservationpj/reservation/controller/ReservationController.java
+++ src/main/java/com/jh/restaurantreservationpj/reservation/controller/ReservationController.java
@@ -1,15 +1,13 @@
 package com.jh.restaurantreservationpj.reservation.controller;
 
 import com.jh.restaurantreservationpj.config.GlobalResponse;
+import com.jh.restaurantreservationpj.reservation.dto.CancelReservationDto;
 import com.jh.restaurantreservationpj.reservation.dto.CreateReservationDto;
 import com.jh.restaurantreservationpj.reservation.service.ReservationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/reservations")
@@ -25,6 +23,16 @@ public class ReservationController {
         String memberId = null;
 
         CreateReservationDto.Response response = reservationService.createReservation(memberId, request);
+
+        return ResponseEntity.ok(GlobalResponse.toGlobalResponse(response));
+    }
+
+    // 회원이 예약 취소하는 컨트롤러
+    @DeleteMapping("/reservation")
+    public ResponseEntity<GlobalResponse<String>> cancel(@Valid @RequestBody CancelReservationDto.Request request) {
+        String memberId = null;
+
+        String response = reservationService.cancelReservation(memberId, request);
 
         return ResponseEntity.ok(GlobalResponse.toGlobalResponse(response));
     }
