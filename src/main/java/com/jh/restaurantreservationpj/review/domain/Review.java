988013dee04c9294bfaa616b7e46cd57e6f06a4d
jinhwan Bea <paewhal20@gmail.com>
2024-05-28 17:51:08 +0900

    Create Review.java
    Feat: 리뷰 엔티티

---
+++ src/main/java/com/jh/restaurantreservationpj/review/domain/Review.java
@@ -0,0 +1,42 @@
+package com.jh.restaurantreservationpj.review.domain;
+
+import com.jh.restaurantreservationpj.config.BaseTimeEntity;
+import com.jh.restaurantreservationpj.member.domain.Member;
+import com.jh.restaurantreservationpj.restaurant.domain.Restaurant;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@SQLDelete(sql = "UPDATE review SET del_date = now() WHERE id=?")
+@SQLRestriction(value = "del_date IS NULL")
+public class Review extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_key", nullable = false)
+    private Restaurant restaurant; // 매장
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_key", nullable = false)
+    private Member member; // 작성자
+
+    @Column(nullable = false)
+    private String title; // 제목
+
+    @Column(length = 5000, nullable = false)
+    private String content; // 내용
+
+    @Column
+    private LocalDateTime delDate; // 삭제 날짜
+}
