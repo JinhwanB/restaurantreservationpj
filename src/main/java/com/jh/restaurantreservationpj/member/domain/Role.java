260955991d0705333129b813e8ccaf731c8c9627
jinhwan Bea <paewhal20@gmail.com>
2024-05-29 17:15:36 +0900

    Update Role.java
    Refector: type 필드 추가

--- src/main/java/com/jh/restaurantreservationpj/member/domain/Role.java
+++ src/main/java/com/jh/restaurantreservationpj/member/domain/Role.java
@@ -1,5 +1,15 @@
 package com.jh.restaurantreservationpj.member.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+// 회원 권한 (admin : 관리자(점장), read : 일반 회원(매장 이용자), write : 리뷰 권한)
+@Getter
+@AllArgsConstructor
 public enum Role {
-    ROLE_ADMIN, ROLE_READ, ROLE_WRITE // 회원 권한 (admin : 관리자(점장), read : 일반 회원(매장 이용자), write : 리뷰 권한)
+    ROLE_ADMIN("ADMIN"),
+    ROLE_READ("READ"),
+    ROLE_WRITE("WRITE");
+
+    private final String type;
 }
