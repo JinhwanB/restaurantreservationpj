81359a864339918543549e3627cc5fe869c75ba0
jinhwan Bea <paewhal20@gmail.com>
2024-05-29 15:26:24 +0900

    Update MemberSignUpDto.java
    Refector: 권한 입력 필드 추가

--- src/main/java/com/jh/restaurantreservationpj/member/dto/MemberSignUpDto.java
+++ src/main/java/com/jh/restaurantreservationpj/member/dto/MemberSignUpDto.java
@@ -3,6 +3,8 @@ package com.jh.restaurantreservationpj.member.dto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
+import java.util.List;
+
 // 회원가입 시 dto
 public class MemberSignUpDto {
 
@@ -17,5 +19,7 @@ public class MemberSignUpDto {
 
         @NotBlank(message = "비밀번호를 입력해주세요.")
         private String password; // 비밀번호
+
+        private List<String> roles; // 권한
     }
 }
