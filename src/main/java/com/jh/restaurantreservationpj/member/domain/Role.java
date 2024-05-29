b60674dabaf74b9e304b79b38d9ba115c559ec9e
jinhwan Bea <paewhal20@gmail.com>
2024-05-29 18:12:13 +0900

    Update Role.java
    Refector: 필드 이름 변경

--- src/main/java/com/jh/restaurantreservationpj/member/domain/Role.java
+++ src/main/java/com/jh/restaurantreservationpj/member/domain/Role.java
@@ -1,15 +1,17 @@
 package com.jh.restaurantreservationpj.member.domain;
 
+import com.jh.restaurantreservationpj.validation.EnumClass;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 // 회원 권한 (admin : 관리자(점장), read : 일반 회원(매장 이용자), write : 리뷰 권한)
 @Getter
 @AllArgsConstructor
+@EnumClass
 public enum Role {
     ROLE_ADMIN("ADMIN"),
     ROLE_READ("READ"),
     ROLE_WRITE("WRITE");
 
-    private final String type;
+    private final String name;
 }
