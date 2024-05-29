4c107d06b8fb0b43cd2806aefadd1259b284cfbd
jinhwan Bea <paewhal20@gmail.com>
2024-05-29 18:57:17 +0900

    Update MemberController.java
    Feat: 로그인 컨트롤러

--- src/main/java/com/jh/restaurantreservationpj/member/controller/MemberController.java
+++ src/main/java/com/jh/restaurantreservationpj/member/controller/MemberController.java
@@ -2,13 +2,18 @@ package com.jh.restaurantreservationpj.member.controller;
 
 import com.jh.restaurantreservationpj.auth.TokenProvider;
 import com.jh.restaurantreservationpj.config.GlobalResponse;
+import com.jh.restaurantreservationpj.member.dto.MemberSignInDto;
 import com.jh.restaurantreservationpj.member.dto.MemberSignUpDto;
 import com.jh.restaurantreservationpj.member.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/auth")
@@ -20,9 +25,21 @@ public class MemberController {
 
     // 회원가입 컨트롤러
     @PostMapping("/signup")
-    public ResponseEntity<GlobalResponse<String>> signUp(MemberSignUpDto.Request request) {
+    public ResponseEntity<GlobalResponse<String>> signUp(@Valid @RequestBody MemberSignUpDto.Request request) {
         String response = memberService.register(request);
 
         return ResponseEntity.ok(GlobalResponse.toGlobalResponse(response));
+    }
+
+    // 로그인 컨트롤러
+    @PostMapping("/signin")
+    public ResponseEntity<GlobalResponse<String>> signIn(@Valid @RequestBody MemberSignInDto.Request request) {
+        MemberSignInDto.Response loginMember = memberService.login(request);
+
+        String userId = loginMember.getUserId();
+        List<String> roles = loginMember.getRoles();
+        String token = tokenProvider.generateToken(userId, roles);
+
+        return ResponseEntity.ok(GlobalResponse.toGlobalResponse(token));
     }
 }
