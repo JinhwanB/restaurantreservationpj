2aec28fed0937c928d11bfd33c8826b7b6a6fb66
jinhwan Bea <paewhal20@gmail.com>
2024-05-29 18:18:07 +0900

    Update MemberErrorCode.java
    Feat: 중복된 회원아이디에 대한 오류 코드 추가

--- src/main/java/com/jh/restaurantreservationpj/member/exception/MemberErrorCode.java
+++ src/main/java/com/jh/restaurantreservationpj/member/exception/MemberErrorCode.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum MemberErrorCode {
+    ALREADY_EXIST_USERID(400, "아이디가 중복입니다."),
     NOT_FOUND_MEMBER(400, "회원으로 등록된 아이디가 아닙니다.");
 
     private final int status;
