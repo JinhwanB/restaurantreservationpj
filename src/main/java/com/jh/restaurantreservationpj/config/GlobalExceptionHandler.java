ead8f213179a265427721ecd9c0f2597168d0828
jinhwan Bea <paewhal20@gmail.com>
2024-05-29 15:28:52 +0900

    Update GlobalExceptionHandler.java
    Feat: 리뷰 관련 exception handler 추가

--- src/main/java/com/jh/restaurantreservationpj/config/GlobalExceptionHandler.java
+++ src/main/java/com/jh/restaurantreservationpj/config/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.jh.restaurantreservationpj.config;
 
 import com.jh.restaurantreservationpj.member.exception.MemberException;
 import com.jh.restaurantreservationpj.restaurant.exception.RestaurantException;
+import com.jh.restaurantreservationpj.review.exception.ReviewException;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
@@ -25,7 +26,7 @@ public class GlobalExceptionHandler {
 
     // 404에러 핸들러
     @ExceptionHandler(NoHandlerFoundException.class)
-    private ResponseEntity<GlobalResponse<?>> handleNotFoundException(NoHandlerFoundException e){
+    private ResponseEntity<GlobalResponse<?>> handleNotFoundException(NoHandlerFoundException e) {
         log.error("404 NotFound = {}", e.getMessage());
 
         return new ResponseEntity<>(GlobalResponse.toGlobalResponseFail(404, "요청하신 페이지를 찾을 수 없습니다."), HttpStatus.NOT_FOUND);
@@ -33,7 +34,7 @@ public class GlobalExceptionHandler {
 
     // 405에러 핸들러
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    private ResponseEntity<GlobalResponse<?>> handle405(HttpRequestMethodNotSupportedException e){
+    private ResponseEntity<GlobalResponse<?>> handle405(HttpRequestMethodNotSupportedException e) {
         log.error("405 error = {}", e.getMessage());
 
         return new ResponseEntity<>(GlobalResponse.toGlobalResponseFail(405, "해당 url을 지원하지 않습니다. Http Method(GET, PUT, POST, DELETE)가 정확한지 확인해주세요."), HttpStatus.METHOD_NOT_ALLOWED);
@@ -41,7 +42,7 @@ public class GlobalExceptionHandler {
 
     // 유효성 검증 에러 핸들러(requestBody) -> 400 에러
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    private ResponseEntity<List<GlobalResponse<?>>> handleValidException(MethodArgumentNotValidException e){
+    private ResponseEntity<List<GlobalResponse<?>>> handleValidException(MethodArgumentNotValidException e) {
         log.error("request 유효성 검사 실패");
 
         List<GlobalResponse<?>> list = new ArrayList<>();
@@ -57,7 +58,7 @@ public class GlobalExceptionHandler {
 
     // 유효성 검증 에러 핸들러(pathVariable, requestParam) -> 400 에러
     @ExceptionHandler(ConstraintViolationException.class)
-    private ResponseEntity<List<GlobalResponse<?>>> handleValidException2(ConstraintViolationException e){
+    private ResponseEntity<List<GlobalResponse<?>>> handleValidException2(ConstraintViolationException e) {
         log.error("pathVariable 또는 requestParam 유효성 검사 실패");
 
         List<GlobalResponse<?>> list = new ArrayList<>();
@@ -72,7 +73,7 @@ public class GlobalExceptionHandler {
 
     // 매장 관련 에러 핸들러 -> 400에러
     @ExceptionHandler(RestaurantException.class)
-    private ResponseEntity<GlobalResponse<?>> handleRestaurantException(RestaurantException e){
+    private ResponseEntity<GlobalResponse<?>> handleRestaurantException(RestaurantException e) {
         log.error("매장 관련 exception = {}", e.getRestaurantErrorCode().getMessage());
 
         return ResponseEntity.badRequest().body(GlobalResponse.toGlobalResponseFail(e.getRestaurantErrorCode().getStatus(), e.getRestaurantErrorCode().getMessage()));
@@ -80,9 +81,17 @@ public class GlobalExceptionHandler {
 
     // 회원 관련 에러 핸들러 -> 400에러
     @ExceptionHandler(MemberException.class)
-    private ResponseEntity<GlobalResponse<?>> handleMemberException(MemberException e){
+    private ResponseEntity<GlobalResponse<?>> handleMemberException(MemberException e) {
         log.error("회원 관련 exception = {}", e.getMemberErrorCode().getMessage());
 
         return ResponseEntity.badRequest().body(GlobalResponse.toGlobalResponseFail(e.getMemberErrorCode().getStatus(), e.getMemberErrorCode().getMessage()));
+    }
+
+    // 리뷰 관련 에러 핸들러 -> 400에러
+    @ExceptionHandler(ReviewException.class)
+    private ResponseEntity<GlobalResponse<?>> handleReviewException(ReviewException e) {
+        log.error("리뷰 관련 exception = {}", e.getReviewErrorCode().getMessage());
+
+        return ResponseEntity.badRequest().body(GlobalResponse.toGlobalResponseFail(e.getReviewErrorCode().getStatus(), e.getReviewErrorCode().getMessage()));
     }
 }
