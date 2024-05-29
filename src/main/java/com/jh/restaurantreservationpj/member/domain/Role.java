83bb661ba6560719ccd27fdec0a10edaf8c76de0
jinhwan Bea <paewhal20@gmail.com>
2024-05-29 18:48:16 +0900

    Update Role.java
    Feat: Enum 검증을 위한 코드 추가

--- src/main/java/com/jh/restaurantreservationpj/member/domain/Role.java
+++ src/main/java/com/jh/restaurantreservationpj/member/domain/Role.java
@@ -1,17 +1,27 @@
 package com.jh.restaurantreservationpj.member.domain;
 
-import com.jh.restaurantreservationpj.validation.EnumClass;
+import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.util.Arrays;
 
 // 회원 권한 (admin : 관리자(점장), read : 일반 회원(매장 이용자), write : 리뷰 권한)
 @Getter
 @AllArgsConstructor
-@EnumClass
 public enum Role {
     ROLE_ADMIN("ADMIN"),
     ROLE_READ("READ"),
     ROLE_WRITE("WRITE");
 
     private final String name;
+
+    // Enum 검증을 위한 코드, Enum에 속하지 않으면 null 리턴
+    @JsonCreator
+    private static Role fromRole(String value) {
+        return Arrays.stream(Role.values())
+                .filter(r -> r.getName().equals(value.toUpperCase()))
+                .findAny()
+                .orElse(null);
+    }
 }
