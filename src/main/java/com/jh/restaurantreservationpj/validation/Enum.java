d859f8cf45ebe6ddc2adf44d74470bdce2455567
jinhwan Bea <paewhal20@gmail.com>
2024-05-29 17:33:54 +0900

    Update Enum.java
    Refector: 제너릭 타입에도 사용 가능하도록 변경

--- src/main/java/com/jh/restaurantreservationpj/validation/Enum.java
+++ src/main/java/com/jh/restaurantreservationpj/validation/Enum.java
@@ -10,7 +10,7 @@ import java.lang.annotation.Target;
 
 // Enum타입을 검증하기 위한 커스텀 어노테이션
 @Constraint(validatedBy = {EnumValidator.class})
-@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.TYPE_USE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Enum {
 
