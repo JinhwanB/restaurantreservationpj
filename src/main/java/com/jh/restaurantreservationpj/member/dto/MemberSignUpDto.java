a12a9f52c067b693146a3da2086aa12ef7c2acb1
jinhwan Bea <paewhal20@gmail.com>
2024-05-29 17:16:04 +0900

    Update MemberSignUpDto.java
    Refector: 권한 필드 타입을 enum타입으로 변경

--- src/main/java/com/jh/restaurantreservationpj/member/dto/MemberSignUpDto.java
+++ src/main/java/com/jh/restaurantreservationpj/member/dto/MemberSignUpDto.java
@@ -1,6 +1,8 @@
 package com.jh.restaurantreservationpj.member.dto;
 
+import com.jh.restaurantreservationpj.member.domain.Role;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 
 import java.util.List;
@@ -20,6 +22,7 @@ public class MemberSignUpDto {
         @NotBlank(message = "비밀번호를 입력해주세요.")
         private String password; // 비밀번호
 
-        private List<String> roles; // 권한
+        @NotEmpty(message = "회원의 권한을 입력해주세요.")
+        private List<Role> roles; // 권한
     }
 }
