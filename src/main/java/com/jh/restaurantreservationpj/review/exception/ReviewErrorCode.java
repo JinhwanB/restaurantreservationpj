e06f5290613bc5a8d9e1da61ee1a3342b344a0fc
jinhwan Bea <paewhal20@gmail.com>
2024-06-03 00:11:57 +0900

    Update ReviewErrorCode.java
    Feat: 관리자가 리뷰 삭제 시 리뷰에 작성된 매장의 관리자가 아닌 경우 에러 코드 추가

--- src/main/java/com/jh/restaurantreservationpj/review/exception/ReviewErrorCode.java
+++ src/main/java/com/jh/restaurantreservationpj/review/exception/ReviewErrorCode.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ReviewErrorCode {
     DIFF_MEMBER(400, "리뷰의 작성자가 아닙니다."),
+    DIFF_MANAGER(400, "리뷰가 작성된 매장의 관리자가 아닙니다."),
     NOT_FOUND_REVIEW(400, "존재하지 않는 리뷰입니다.");
 
     private final int status;
