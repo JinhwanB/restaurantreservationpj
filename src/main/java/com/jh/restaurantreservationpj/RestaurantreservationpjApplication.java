808f06615907e039c1a11c285bcd9b584e725c03
jinhwan Bea <paewhal20@gmail.com>
2024-06-04 19:28:09 +0900

    Update RestaurantreservationpjApplication.java
    Feat: cache 사용 활성화

--- src/main/java/com/jh/restaurantreservationpj/RestaurantreservationpjApplication.java
+++ src/main/java/com/jh/restaurantreservationpj/RestaurantreservationpjApplication.java
@@ -2,16 +2,18 @@ package com.jh.restaurantreservationpj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableAspectJAutoProxy
+@EnableCaching
 public class RestaurantreservationpjApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestaurantreservationpjApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantreservationpjApplication.class, args);
+    }
 
 }
