fe792e76781dbd2fb1e32f304afd8472fd49c77e
jinhwan Bea <paewhal20@gmail.com>
2024-06-04 20:37:41 +0900

    Update CacheKey.java
    Feat: 리뷰 캐시 저장 시 value로 사용할 키값 필드 추가

--- src/main/java/com/jh/restaurantreservationpj/config/CacheKey.java
+++ src/main/java/com/jh/restaurantreservationpj/config/CacheKey.java
@@ -4,4 +4,5 @@ package com.jh.restaurantreservationpj.config;
 public class CacheKey {
     public static final String RESERVATION_KEY = "reservation";
     public static final String RESTAURANT_KEY = "restaurant";
+    public static final String REVIEW_KEY = "review";
 }
